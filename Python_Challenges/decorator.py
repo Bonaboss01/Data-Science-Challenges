3e3108d393f799861820f05aa52671535b3df94f
Bonaventure Osuide <bonaventureosuide@gmail.com>
2025-11-19 17:51:16 +0000

    Update decorator.py

--- Python_Challenges/decorator.py
+++ Python_Challenges/decorator.py
@@ -7,5 +7,12 @@ def say_hello():
   print("Hello!")
 say_hello = my_decorator(say_hello)
 
+# Decorators with arguments
+
+@app.route("/")
+@app.route("/hello/<name>")
+@app.errorhandler(404)
+
+# Each of them registers a function with Flask
 
 
