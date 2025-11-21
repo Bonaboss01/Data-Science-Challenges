2dae85dc3d97374c40c3b508916c39faa5a6fbad
Bonaventure Osuide <bonaventureosuide@gmail.com>
2025-11-21 23:30:51 +0000

    add a code to create a class to represent time of the day

--- Python_Challenges/object_class.py
+++ Python_Challenges/object_class.py
@@ -34,3 +34,45 @@ point1 = Point2D(3, 4)
 point2 = Point2D(9, 5)
 distance = point1.calculate_distance(point2)
 print(distance)
+
+
+# create a class to represent time of the day
+class Time():
+    
+    def __init__(self, hour, minute, second):
+        """
+        Initialize a Time instance giving the hour, minute and second.
+        """
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+        
+    def total_seconds(self):
+        """
+        Compute the total number of seconds since the start of the day.
+        """
+        return 3600 * self.hour + 60 * self.minute + self.second
+    
+    def __str__(self):
+        """
+        Create a string representation of this time.
+        """
+        s = ''
+        if self.hour < 10:
+            s += '0'
+        s += str(self.hour)
+        s += ':'
+        if self.minute < 10:
+            s += '0'
+        s += str(self.minute)
+        s += ':'
+        if self.second < 10:
+            s += '0'
+        s += str(self.second)
+        return s
+
+# Solution testing
+my_time = Time(9, 5, 7)
+my_seconds = my_time.total_seconds()
+print(my_seconds)
+print(my_time)
