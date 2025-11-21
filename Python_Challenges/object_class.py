c66983078524400b5fdcec1c323ec2bf4f87da7c
Bonaventure Osuide <bonaventureosuide@gmail.com>
2025-11-21 23:28:35 +0000

    add class object to represent points in two dimensions

--- Python_Challenges/object_class.py
+++ Python_Challenges/object_class.py
@@ -13,3 +13,24 @@ class Person():
 
 person = Person("EmiLia", "GomEZ")
 print(person)
+
+
+# create a class to represent points in two dimensions
+
+import math
+class Point2D():
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+    def calculate_distance(self, other):
+        dx = self.x - other.x
+        dy = self.y - other.y
+        return math.sqrt(dx * dx + dy * dy)
+
+# Solution testing
+point1 = Point2D(3, 4)
+point2 = Point2D(9, 5)
+distance = point1.calculate_distance(point2)
+print(distance)
