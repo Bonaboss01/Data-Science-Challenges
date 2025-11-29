c797fdfad400a5357e5f4f0d7abe7ebccc11d169
Bonaventure Osuide <bonaventureosuide@gmail.com>
2025-11-29 08:15:26 +0000

    Update date_time.py using pytz library

--- Python_Challenges/date_time.py
+++ Python_Challenges/date_time.py
@@ -1,3 +1,4 @@
+# Investigating with different timedate packages
 import datetime as dt
 time_of_year = dt.datetime(year, month, day, hour, minutes, seconds)
 
@@ -5,4 +6,11 @@ time_to_format = dt.datetime(2020, 1, 25, 15, 25, 13)
 
 print(time_to_format.strftime("%Y/%m/%d %H:%M:%S"))
 
-  
+time_str = "12:30 2019-05-19"
+
+date_object = dt.datetime.strptime(time_str, "%H:%M %Y-%m-%d")
+
+import pytz
+
+my_timezone_name = 'America/New_York' 
+time_now = dt.datetime.now(pytz.timezone(my_timezone_name))
