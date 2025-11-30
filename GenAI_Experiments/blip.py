bb0cd8873bbd464af9d726e5e49f6c57399ef767
Bonaventure Osuide <bonaventureosuide@gmail.com>
2025-11-30 05:02:57 +0000

    Update blip.py

--- GenAI_Experiments/blip.py
+++ GenAI_Experiments/blip.py
@@ -16,3 +16,22 @@ def generate_caption(image_path):
 
 print(generate_caption("image.jpg"))
 
+
+from transformers import BlipProcessor, BlipForConditionalGeneration
+from PIL import Image
+import torch
+
+# Load once
+processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base")
+
+def generate_caption(image_path):
+    image = Image.open(image_path)
+    inputs = processor(image, return_tensors="pt")
+    outputs = model.generate(**inputs)
+    caption = processor.decode(outputs[0], skip_special_tokens=True)
+    return caption
+
+print("Caption:", generate_caption("path_to_your_image.jpg"))
+
+
