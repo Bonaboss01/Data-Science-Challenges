2fd9b706be0a0089e72ee25e806f9c88686ca68b
Bonaventure Osuide <bonaventureosuide@gmail.com>
2026-02-04 16:18:55 +0000

    Update general.py

--- Python_Challenges/general.py
+++ Python_Challenges/general.py
@@ -3,6 +3,8 @@ import pandas as pd
 df = pd.read_csv("data.csv")
 print("Rows:", len(df))
 print(df.isnull().sum())
+print(df.describe())
+print
 # rename columns to snake_case
 df.columns = df.columns.str.lower().str.replace(" ", "_")
 df.to_csv("cleaned.csv", index=False)
