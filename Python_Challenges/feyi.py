c2377b6d72251d11a6af190aeebca86938eb5be7
Bonaventure Osuide <bonaventureosuide@gmail.com>
2026-02-05 23:51:10 +0000

    Create feyi.py

---
+++ Python_Challenges/feyi.py
@@ -0,0 +1,11 @@
+def love_message(name):
+    return f"{name}, you are my today and all my tomorrows ❤️"
+start_year = 2018
+proposal_year = 2026
+marriage_year = 2027
+
+print(f"Dating since: {start_year}")
+print(f"Proposal: {proposal_year}")
+print(f"Marriage: {marriage_year}")
+if __name__ == "__main__":
+    print(love_message("Feyi"))
