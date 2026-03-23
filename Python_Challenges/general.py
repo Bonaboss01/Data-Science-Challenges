a95730a4c29ba02bcb47dfb091c1eeef2c3b6911
Bonaventure Osuide <bonaventureosuide@gmail.com>
2026-03-23 22:26:58 +0100

    Update general.py

--- Python_Challenges/general.py
+++ Python_Challenges/general.py
@@ -14,3 +14,13 @@ df.to_csv("cleaned.csv", index=False)
 with open("app.log") as f:
   print("lines:", len(f.readlines()))
 
+
+
+
+def validate_dataset(df):
+    assert df["date"].isnull().sum() == 0, "Missing dates found"
+    assert df["store_id"].isnull().sum() == 0, "Missing store_id found"
+    assert df["product_id"].isnull().sum() == 0, "Missing product_id found"
+    assert (df["units_sold"] >= 0).all(), "Negative sales detected"
+    
+    return True
